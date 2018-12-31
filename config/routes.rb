406373253c9ef889db987408876a5ee0d55d7647
koudai.suzuki <koudai6456@icloud.com>
2018-12-31 06:48:55 +0000

    message

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :welcome, :only => [:index]
+  root "welcome#index"
+  resources :users, :only =>[:index, :show, :edit, :update]
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
